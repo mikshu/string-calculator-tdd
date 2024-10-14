e7e554e5a8b51550aca87ea7b075ccae4e4d76f1
minhasaatish <minhasaatish@gmail.com>
2024-10-14 18:24:50 +0530

    Added negative number case if found any in numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,10 +2,21 @@
 function add(numbers) {
   // return 0 if input is an empty string
   if (numbers === "") return 0;
+
   //replace new line with a comma
   const normalizedNumbers = numbers.replace(/\n/g, ",");
-  //make an array of numbers
-  const numArr = normalizedNumbers.split(",");
+
+  // Split the string by comma
+  const numArr = normalizedNumbers.split(",").map(Number);
+
+  //filter an Array for negative numbers
+  const negativesNumArr = numArr.filter((n) => n < 0);
+
+  if (negativesNumArr.length > 0) {
+    throw new Error(
+      `Negative numbers not allowed: ${negativesNumArr.join(",")}`
+    );
+  }
   return numArr.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -16,3 +16,7 @@ test("should return the sum of two comma-separated number", () => {
 test("should handle new line b/w numbers", () => {
   expect(add("1\n2,3")).toBe(6);
 });
+
+test("should throw an error for negative numbers", () => {
+  expect(() => add("1,-2,3,-4")).toThrow("Negative numbers not allowed: -2,-4");
+});
